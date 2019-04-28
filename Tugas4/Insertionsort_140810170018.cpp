2842df68f823a3ceae9cc2dde4110efbfe308f12
feliasya <43400930+feliasya@users.noreply.github.com>
2019-04-29 00:19:24 +0700

    menambahkan tugas

---
+++ Tugas4/Insertionsort_140810170018.cpp
@@ -0,0 +1,43 @@
+/*
+Nama program : Insertion sort
+Nama : Felia Sri Indriyani
+NPM : 140810170018
+*/
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int i,j,n,temp,a[30];
+    cout<<"Enter the number of elements:";
+    cin>>n;
+    cout<<"\nEnter the elements\n";
+ 
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+ 
+    for(i=1;i<=n-1;i++)
+    {
+        temp=a[i];
+        j=i-1;
+ 
+        while((temp<a[j])&&(j>=0))
+        {
+            a[j+1]=a[j];    //moves element forward
+            j=j-1;
+        }
+ 
+        a[j+1]=temp;    //insert element in proper place
+    }
+ 
+    cout<<"\nSorted list is as follows by Insertion sort\n";
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+ 
+    return 0;
+}
